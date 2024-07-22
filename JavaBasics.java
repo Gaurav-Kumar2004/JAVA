47c5830670aff61b4c05fdc9e4449a8719e4ca3d
Gaurav Kumar <gauravkumarhjp2004@gmail.com>
2024-07-22 21:17:37 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics {
+
+    public static void main(String[] args) {
+        System.out.println("Enter Two no");
+        Scanner obj = new Scanner(System.in);
+
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int sum = a+b;
+        System.out.println("Sum = "+sum);
+    }
+}
